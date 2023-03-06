a9060a5583d1bbbfe0a0de6760fd5bf3a5be2876
Kien Vu <kvstudio.154@gmail.com>
2023-03-07 00:26:54 +0700

    add login ui

--- src/main.js
+++ src/main.js
@@ -2,14 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
+import VueConfirmDialog from "vue-confirm-dialog";
 import * as bootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
+Vue.use(VueConfirmDialog);
 Vue.use(bootstrapVue.IconsPlugin);
 Vue.use(bootstrapVue.BootstrapVue);
 
 Vue.config.productionTip = false;
+
+Vue.component("vue-confirm-dialog", VueConfirmDialog.default);
 
 new Vue({
   router,
